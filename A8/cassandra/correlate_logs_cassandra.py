9695957b8c8f1d25c1464836903caab0b0c3db13
chhokara <chhokara@gmail.com>
2024-10-28 19:02:07 -0700

    load data into cassandra

---
+++ A8/cassandra/correlate_logs_cassandra.py
@@ -0,0 +1,45 @@
+from math import sqrt
+from pyspark.sql import SparkSession, functions as F, types
+import sys
+assert sys.version_info >= (3, 5)  # make sure we have Python 3.5+
+
+
+def main(keyspace, table):
+    nasa_logs = spark.read.format("org.apache.spark.sql.cassandra") \
+        .options(table=table, keyspace=keyspace).load()
+    nasa_logs = nasa_logs.groupBy(nasa_logs['host']).agg(F.count(
+        'host').alias('count_requests'), F.sum('bytes').alias('sum_request_bytes'))
+    nasa_logs = nasa_logs.withColumn("one", F.lit(1))
+    nasa_logs = nasa_logs.withColumn("xi", F.col("count_requests"))
+    nasa_logs = nasa_logs.withColumn(
+        "xi_squared", F.pow(F.col('count_requests'), 2))
+    nasa_logs = nasa_logs.withColumn("yi", F.col("sum_request_bytes"))
+    nasa_logs = nasa_logs.withColumn(
+        "yi_squared", F.pow(F.col('sum_request_bytes'), 2))
+    nasa_logs = nasa_logs.withColumn("xi_yi", F.col(
+        "count_requests") * F.col("sum_request_bytes"))
+
+    n = nasa_logs.agg(F.sum('one')).collect()[0][0]
+    sum_xi_yi = nasa_logs.agg(F.sum('xi_yi')).collect()[0][0]
+    sum_xi = nasa_logs.agg(F.sum('xi')).collect()[0][0]
+    sum_yi = nasa_logs.agg(F.sum('yi')).collect()[0][0]
+    sum_xi_squared = nasa_logs.agg(F.sum('xi_squared')).collect()[0][0]
+    sum_yi_squared = nasa_logs.agg(F.sum('yi_squared')).collect()[0][0]
+
+    r = ((n * sum_xi_yi) - (sum_xi * sum_yi)) / (sqrt((n * sum_xi_squared) -
+                                                      (sum_xi) ** 2) * sqrt((n * sum_yi_squared) - (sum_yi) ** 2))
+
+    print("r: ", r)
+    print("r squared: ", r ** 2)
+
+
+if __name__ == "__main__":
+    keyspace = sys.argv[1]
+    table = sys.argv[2]
+    cluster_seeds = ['node1.local', 'node2.local']
+    spark = SparkSession.builder.appName('Load logs into Cassandra').config(
+        'spark.cassandra.connection.host', ','.join(cluster_seeds)).getOrCreate()
+    assert spark.version >= '3.0'  # make sure we have Spark 3.0+
+    spark.sparkContext.setLogLevel('WARN')
+    sc = spark.sparkContext
+    main(keyspace, table)
---
+++ A8/cassandra/load_logs_spark.py
@@ -0,0 +1,51 @@
+from datetime import datetime
+import uuid
+from pyspark.sql import SparkSession, functions as F, types
+import sys
+import re
+assert sys.version_info >= (3, 5)  # make sure we have Python 3.5+
+
+
+def parse_log(line):
+    line_re = re.compile(
+        r'^(\S+) - - \[(\S+) [+-]\d+\] \"[A-Z]+ (\S+) HTTP/\d\.\d\" \d+ (\d+)$')
+    parsed = line_re.match(line)
+    if parsed:
+        return {
+            "id": str(uuid.uuid4()),
+            "host": parsed.group(1),
+            "datetime": datetime.strptime(parsed.group(2), "%d/%b/%Y:%H:%M:%S").strftime("%Y-%m-%d %H:%M:%S"),
+            "path": parsed.group(3),
+            "bytes": int(parsed.group(4))
+        }
+    return None
+
+
+def main(input, keyspace, table):
+    nasa_logs = sc.textFile(input)
+    nasa_logs = nasa_logs.map(parse_log).filter(lambda log: log != None)
+
+    log_schema = types.StructType([
+        types.StructField('id', types.StringType()),
+        types.StructField('host', types.StringType()),
+        types.StructField('datetime', types.StringType()),
+        types.StructField('path', types.StringType()),
+        types.StructField('bytes', types.IntegerType())
+    ])
+
+    nasa_logs = spark.createDataFrame(nasa_logs, log_schema)
+    nasa_logs.write.format("org.apache.spark.sql.cassandra").options(
+        table=table, keyspace=keyspace).mode("append").save()
+
+
+if __name__ == "__main__":
+    input = sys.argv[1]
+    keyspace = sys.argv[2]
+    table = sys.argv[3]
+    cluster_seeds = ['node1.local', 'node2.local']
+    spark = SparkSession.builder.appName('Load logs into Cassandra').config(
+        'spark.cassandra.connection.host', ','.join(cluster_seeds)).getOrCreate()
+    assert spark.version >= '3.0'  # make sure we have Spark 3.0+
+    spark.sparkContext.setLogLevel('WARN')
+    sc = spark.sparkContext
+    main(input, keyspace, table)
